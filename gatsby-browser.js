c5a2580bfae8be6bcc570825ec1684887f876e76
pauliescanlon <pauliescanlon@gmail.com>
2021-03-09 07:17:27 +0000

    feat: reset global.css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PageLayout from './src/layouts/page-layout'
 import './src/styles/global.css'
+import './src/styles/styles.css'
 
 export const wrapPageElement = ({ element }) => {
   return <PageLayout>{element}</PageLayout>
